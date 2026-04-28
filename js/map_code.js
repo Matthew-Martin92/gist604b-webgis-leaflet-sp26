d2b1f96cd9d86ebdb0d099f1f9b9ef08f83e4008
Matthew_Martin <mmartin10@arizona.edu>
2026-04-27 21:10:07 -0700

    Updated map code with data layer groups and point data and styling

--- js/map_code.js
+++ js/map_code.js
@@ -7,7 +7,7 @@
 
 //Create Leaflet map in the "map" div
 // setView([lat, lon], zoom)
-const map = L.map('map').setView([34.0336,-117.0431], 50)
+var map = L.map('map').setView([34.0296,-117.0631], 13.5);
 
 // ================================================
 //Step 2: Add Basemap
@@ -17,3 +17,58 @@ const map = L.map('map').setView([34.0336,-117.0431], 50)
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+
+// ================================================
+//Step 3: Add/Customize controls
+// ================================================
+
+// Move Zoom buttons (defult + topleft)
+map.zoomControl.setPosition('topright');
+
+// add a scale bar
+L.control.scale().addTo(map);
+
+// ================================================
+//Step 4: Create layer groups
+// ================================================
+
+// Layer groups allow toggling layers on/off
+const lineLayer = L.layerGroup().addTo(map);
+const pointLayer = L.layerGroup().addTo(map);
+const polygonLayer = L.layerGroup().addTo(map);
+
+// UI to toggle layers
+L.control.layers(null, {
+    'Streets': lineLayer,
+    'Schools': pointLayer,
+    'Land Use': polygonLayer
+}).addTo(map);
+
+// ================================================
+//Step 5: Load Point Data (Schools)
+// ================================================
+fetch('data/yucaipa_schools.geojson')
+    .then(res => res.json())
+    .then(data => {L.geoJSON(data, {
+        //Style points as 
+        pointToLayer: function(feature, latlng) {
+            return L.circleMarker(latlng,{
+                radius: 6,
+                fillColor: '#3724e7',
+                color: '#ffffff',
+                weight: 1,
+                fillOpacity: 0.9,
+            })
+        },
+
+        //add popups
+        onEachFeature: function(feature, layer) {
+            const name = feature.properties.name || 'School';
+            layer.bindPopup(`<strong>${name}</strong>`);
+        }
+    }).addTo(pointLayer);
+})
+.catch(err => console.error('Error loading schools'))
+
+
